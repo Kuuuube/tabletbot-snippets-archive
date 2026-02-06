b03b37e439f170f5fff3a711c79d272ab63092b5
kuuuube <hexagonisalie@gmail.com>
2026-02-06 00:52:21 -0500

    Generate txt files off json

--- format.sh
+++ format.sh
@@ -1,1 +1,5 @@
 find ./json/*.json -type f -exec sh -c 'cat <<< $(jq . "{}") > "{}"' \;
+find ./json/*.json -type f \
+    -exec sh -c 'jq -r .rawTitle {} | sed '\''s/^/**/; s/$/**/'\'' > ./txt/$(basename {} .json).txt' \; \
+    -exec sh -c 'echo >> ./txt/$(basename {} .json).txt' \; \
+    -exec sh -c 'jq -r .rawDescription {} | sed '\''s/\\n/\n/g'\'' >> ./txt/$(basename {} .json).txt' \;
